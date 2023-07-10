8acfa0a0f5864fca4e89981a97b9bab606ac03ac
Bindu3118 <136918241+Bindu3118@users.noreply.github.com>
2023-07-10 13:17:18 -0400

    Update dependencies.tf

--- asg-elb-service/dependencies.tf
+++ asg-elb-service/dependencies.tf
@@ -8,7 +8,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet" "vpcsubnet" {
+data "aws_subnet" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
